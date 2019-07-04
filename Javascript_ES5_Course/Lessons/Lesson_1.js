43df7449a9e110d05e8b3c4dfda80ee998f924f8
Prokhor Zimon <prohor.z@kultprosvet.net>
2019-07-04 17:04:03 +0300

    add webpack

---
+++ Javascript_ES5_Course/Lessons/Lesson_1.js
@@ -0,0 +1,4 @@
+// Devtools console message ✨
+console.log('%cLesson 1!', 'color: red; font-size: 25px; font-weight: bold;');
+
+
---
+++ Javascript_ES5_Course/Lessons/__webpack__/webpack.config.js
@@ -0,0 +1,34 @@
+const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+    context: path.resolve(__dirname, 'public'),
+    entry: path.resolve(__dirname, 'public/index.js'),
+	output: {
+		path: path.resolve(__dirname, 'dist')
+	},
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        compress: true,
+        quiet: true
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+			template: './index.html'
+		}),
+        new BrowserSyncPlugin({
+			server: {
+				baseDir: ['dist']
+			},
+			port: 3000,
+			host: 'localhost',
+			open: false,
+			files: [
+				{
+					//match: [ '../*.js'],
+				}
+			]
+		})
+    ]
+}
