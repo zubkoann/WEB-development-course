67159ed157ed7676d566d7ae84704827f6e97d32
ft101586zaa <annazubko1986@gmail.com>
2019-07-15 13:03:15 +0300

    add homework lesson_2 Zubko Anna

---
+++ Javascript_ES5_Course/Homework/Lesson_2/annazubko.js
@@ -0,0 +1,9 @@
+function poligonSquare(n) {
+	return (2*Math.pow(n, 2) - 2*n + 1);
+}
+
+// TEST
+console.log(poligonSquare(1)); //1
+console.log(poligonSquare(2)); //5
+console.log(poligonSquare(3)); //13
+console.log(poligonSquare(4)); //25
