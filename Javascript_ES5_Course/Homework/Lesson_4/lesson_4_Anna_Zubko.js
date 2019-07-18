a75f03a834a900d93650443c1bd950e1db928255
ft101586zaa <annazubko1986@gmail.com>
2019-07-18 14:21:15 +0300

    added lesson3 homework

--- Javascript_ES5_Course/Homework/Lesson_4/lesson_4_Anna_Zubko.js
+++ Javascript_ES5_Course/Homework/Lesson_4/lesson_4_Anna_Zubko.js
@@ -1,15 +1,19 @@
-
 function checkArr(arr){
-let newarr = []; 
-arr.forEach((el,i) =>{
-    if (i==0) newarr.push(el)
-    if (el>=arr[i-1] && i>0) newarr.push(el); 
-
-});
-let b = (arr.length == newarr.length || arr.length - 1  == newarr.length) ? true :  false;
-return b;
-}
-
-// TEST 
-console.log(checkArr([1,2,33, 3, 3, 2]));
-console.log(checkArr([1,2, 3, 3, 2]));
+    let newarr = [];
+    let count = 0;
+    arr.forEach((el,i) =>{
+       if (el>newarr[newarr.length-1] || (newarr.length == 0 && el<= arr[i+1]) || (el>newarr[newarr.length-1] && el> arr[i+1]) ) newarr.push(el)
+       else count++;  
+    //    if (count>1) return false;     
+    });
+    console.log(newarr);
+    return (arr.length == newarr.length || arr.length - 1  == newarr.length) ? true :  false;
+    }
+    
+    // TEST
+    console.log(checkArr([1,2,1, 2]));
+    console.log(checkArr([1,1, 2, 3,3,4,4]));
+    console.log(checkArr([10, 1, 2, 3, 4, 5]));
+    console.log(checkArr([0, -2, 5, 6]));
+    console.log(checkArr([1, 2, 5, 3, 5]));
+    console.log(checkArr([0,-2, -2, 5, 6]));
