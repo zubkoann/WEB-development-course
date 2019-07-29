58a8ce5f0fab0478c31e54edc680a2660427e592
ft101586zaa <annazubko1986@gmail.com>
2019-07-29 10:37:53 +0300

    lesson7

--- Javascript_ES5_Course/Homework/Lesson_7/Lesson_4_Anna_Zubko.js
+++ Javascript_ES5_Course/Homework/Lesson_7/Lesson_4_Anna_Zubko.js
@@ -1,27 +1,29 @@
 function checkIfArraySame(array1, array2){
     let count = 0;
-    // let array1=aarray1;
-    // let array2=aarray2;
-
+    if (array1.length != array2.length)return false;
     array1.forEach((element, i) => {
-        if(element == array2[i]) return false;
+        if(element == array2[i]){
+            return false;
+        } 
+        else if (count<=1){
         for (let j=i+1; j<array2.length; j++) {
-            if(element == array2[j] && count<1){
+            if(element == array2[j]){
+                    let arr2_spliced_el = array2.splice(i, 1, element)[0];
+                    array2.splice(j, 1, arr2_spliced_el);
                     count++;
-                    var array2_el = array2.splice(i, 1, element);
-                    array2.splice(j, 1, array2_el);
-                    console.log('sec',array2, i)
-                    return false;
-            }else{
-                count++;
             }
         }
+    }
     });
-    return (count>1)? false:true;
+    return (array1.join(',') == array2.join(','))? true: false;
 }
+
 
 // TEST 
 console.log(checkIfArraySame([1, 2, 3], [1, 2, 3]))
 console.log(checkIfArraySame([1, 2, 3], [2, 1, 3]))
+console.log(checkIfArraySame([1, 2, 3, 4, 5], [2, 1, 3, 4, 4]))
+console.log(checkIfArraySame([2, 1, 1, 2, 3, 4], [1, 1, 2, 1, 3, 4]))
 console.log(checkIfArraySame([1, 2, 2], [2, 1, 1]))
 console.log(checkIfArraySame([1, 3, 2, 3], [2, 3, 1, 3]))
+console.log(checkIfArraySame([1, 3, 2, 3], [2, 3, 1, 3, 4]))
